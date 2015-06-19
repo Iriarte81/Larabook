10a37366e68c311363bf24faaa0c7fc30ca08b29
Jose Iriarte <josemariairiarte@gmail.com>
2015-06-19 18:49:57 -0300

    Dependencies, Database Configuration, The Master Page

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use View;
+
+class PagesController extends Controller
+{
+ public function home() {
+
+    return View::make('pages.home');
+
+ } 
+}
